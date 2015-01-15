eabed53d6ec6bdf90b2188d95fa1a4bc527dd88c
wandersonrs <wandersons.roo@hotmail.com>
2015-01-15 12:15:31 -0300

    Criação da tela InserirEditarDespesa Wanderson

---
+++ src/telas/JInserirEditarDespesa.java
@@ -0,0 +1,134 @@
+package telas;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JFormattedTextField;
+import javax.swing.JComboBox;
+import javax.swing.JTextArea;
+import javax.swing.JTable;
+import java.awt.Choice;
+import java.awt.TextArea;
+import java.awt.Component;
+import javax.swing.Box;
+import java.awt.Scrollbar;
+import javax.swing.JScrollBar;
+import javax.swing.JSeparator;
+import javax.swing.SwingConstants;
+import javax.swing.JButton;
+import javax.swing.ImageIcon;
+import com.toedter.calendar.JDateChooser;
+import com.toedter.components.JSpinField;
+
+public class JInserirEditarDespesa extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					JInserirEditarDespesa frame = new JInserirEditarDespesa();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public JInserirEditarDespesa() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 452, 392);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		textField = new JTextField();
+		textField.setToolTipText("Clique para inserir o nome para despesa.");
+		textField.setBounds(135, 35, 194, 20);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblNomeDaDespesa = new JLabel("Nome da despesa:");
+		lblNomeDaDespesa.setFont(new Font("Times New Roman", Font.PLAIN, 13));
+		lblNomeDaDespesa.setBounds(28, 38, 114, 14);
+		contentPane.add(lblNomeDaDespesa);
+		
+		JLabel lblVencimento = new JLabel("Vencimento:");
+		lblVencimento.setFont(new Font("Times New Roman", Font.PLAIN, 13));
+		lblVencimento.setBounds(28, 75, 79, 14);
+		contentPane.add(lblVencimento);
+		
+		JLabel lblDescrioDaDespesa = new JLabel("Descri\u00E7\u00E3o da despesa:");
+		lblDescrioDaDespesa.setFont(new Font("Times New Roman", Font.PLAIN, 13));
+		lblDescrioDaDespesa.setBounds(28, 107, 126, 14);
+		contentPane.add(lblDescrioDaDespesa);
+		
+		TextArea textArea = new TextArea();
+		textArea.setBounds(160, 107, 234, 74);
+		contentPane.add(textArea);
+		
+		JLabel lblQuantidadeDeParcelas = new JLabel("Quantidade de parcelas:");
+		lblQuantidadeDeParcelas.setFont(new Font("Times New Roman", Font.PLAIN, 13));
+		lblQuantidadeDeParcelas.setBounds(28, 212, 144, 14);
+		contentPane.add(lblQuantidadeDeParcelas);
+		
+		JLabel lblValorR = new JLabel("Valor R$:");
+		lblValorR.setFont(new Font("Times New Roman", Font.PLAIN, 13));
+		lblValorR.setBounds(28, 251, 60, 14);
+		contentPane.add(lblValorR);
+		
+		JFormattedTextField formattedTextField = new JFormattedTextField();
+		formattedTextField.setToolTipText("Insira o valor da despesa em reais. N\u00E3o pode ser valores negativos.");
+		formattedTextField.setBounds(98, 248, 85, 20);
+		contentPane.add(formattedTextField);
+		
+		JSeparator separator = new JSeparator();
+		separator.setBounds(0, 288, 438, 8);
+		contentPane.add(separator);
+		
+		JButton btnCancelar = new JButton("Ok");
+		btnCancelar.setToolTipText("Clique para confirmar as edi\u00E7\u00F5es realizadas.");
+		btnCancelar.setBounds(240, 307, 89, 23);
+		contentPane.add(btnCancelar);
+		
+		JButton btnCancelar_1 = new JButton("Cancelar");
+		btnCancelar_1.setToolTipText("Clique para cancelar as edi\u00E7\u00F5es realizadas.");
+		btnCancelar_1.setBounds(135, 307, 89, 23);
+		contentPane.add(btnCancelar_1);
+		
+		JButton btnSair = new JButton("Sair");
+		btnSair.setToolTipText("Clique para sair da tela.");
+		btnSair.setBounds(339, 307, 89, 23);
+		contentPane.add(btnSair);
+		
+		JDateChooser dateChooser = new JDateChooser();
+		dateChooser.setToolTipText("Selecione a data de vencimento da despesa.");
+		dateChooser.setBounds(117, 69, 87, 20);
+		contentPane.add(dateChooser);
+		
+		JSpinField spinField = new JSpinField();
+		spinField.setToolTipText("Selecione a quantidade de parcelas desta despesa, caso necess\u00E1rio.");
+		spinField.setBounds(160, 212, 29, 20);
+		contentPane.add(spinField);
+		
+		JButton btnNewButton = new JButton("New button");
+		btnNewButton.setBounds(18, 307, 89, 23);
+		contentPane.add(btnNewButton);
+	}
+}
