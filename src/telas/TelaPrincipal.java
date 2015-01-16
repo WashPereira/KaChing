fbeb3f526fad642b9e50f965f42d56da125e0c6a
wash <washpereiraa@gmail.com>
2015-01-16 18:22:06 -0300

    Alterações nas telas

--- src/telas/TelaPrincipal.java
+++ src/telas/TelaPrincipal.java
@@ -12,10 +12,6 @@ import javax.swing.JLabel;
 import javax.swing.ImageIcon;
 import javax.swing.SwingConstants;
 import com.toedter.calendar.JCalendar;
-import com.jgoodies.forms.layout.FormLayout;
-import com.jgoodies.forms.layout.ColumnSpec;
-import com.jgoodies.forms.factories.FormFactory;
-import com.jgoodies.forms.layout.RowSpec;
 
 
 public class TelaPrincipal extends JFrame {
@@ -70,12 +66,6 @@ public class TelaPrincipal extends JFrame {
 		panel_1.setToolTipText("");
 		panel_1.setBounds(10, 274, 242, 216);
 		contentPane.add(panel_1);
-		panel_1.setLayout(new FormLayout(new ColumnSpec[] {
-				ColumnSpec.decode("25px"),
-				ColumnSpec.decode("191px"),},
-			new RowSpec[] {
-				RowSpec.decode("21px"),
-				RowSpec.decode("153px"),}));
 		
 		JCalendar calendar = new JCalendar();
 		panel_1.add(calendar, "2, 2, left, top");
@@ -84,54 +74,47 @@ public class TelaPrincipal extends JFrame {
 		panel_2.setBorder(new TitledBorder(null, "Controle do Caixa", TitledBorder.LEADING, TitledBorder.TOP, null, null));
 		panel_2.setBounds(270, 11, 931, 479);
 		contentPane.add(panel_2);
-		panel_2.setLayout(new FormLayout(new ColumnSpec[] {
-				FormFactory.UNRELATED_GAP_COLSPEC,
-				ColumnSpec.decode("145px"),
-				ColumnSpec.decode("635px"),
-				ColumnSpec.decode("114px"),},
-			new RowSpec[] {
-				RowSpec.decode("22px"),
-				RowSpec.decode("52px"),
-				FormFactory.UNRELATED_GAP_ROWSPEC,
-				RowSpec.decode("52px"),
-				FormFactory.RELATED_GAP_ROWSPEC,
-				RowSpec.decode("52px"),
-				FormFactory.UNRELATED_GAP_ROWSPEC,
-				RowSpec.decode("50px"),
-				FormFactory.UNRELATED_GAP_ROWSPEC,
-				RowSpec.decode("57px"),}));
+		panel_2.setLayout(null);
 		
 		JLabel InserirReceita = new JLabel("Inserir Receita");
-		panel_2.add(InserirReceita, "2, 2, fill, fill");
+		InserirReceita.setBounds(11, 21, 150, 50);
+		panel_2.add(InserirReceita);
 		InserirReceita.setIcon(new ImageIcon(TelaPrincipal.class.getResource("/img/plus+.png")));
 		
 		JLabel InserirDespesas = new JLabel("Inserir Despesas");
-		panel_2.add(InserirDespesas, "2, 4, fill, fill");
+		InserirDespesas.setBounds(184, 21, 160, 50);
+		panel_2.add(InserirDespesas);
 		InserirDespesas.setIcon(new ImageIcon(TelaPrincipal.class.getResource("/img/rounded61.png")));
 		
 		JLabel InserirMetas = new JLabel("Inserir Metas");
-		panel_2.add(InserirMetas, "2, 6, fill, bottom");
+		InserirMetas.setBounds(354, 21, 150, 50);
+		panel_2.add(InserirMetas);
 		InserirMetas.setIcon(new ImageIcon(TelaPrincipal.class.getResource("/img/alvo.png")));
 		
 		JLabel PouparDinheiro = new JLabel("Poupar Dinheiro");
-		panel_2.add(PouparDinheiro, "2, 8, left, center");
+		PouparDinheiro.setBounds(514, 21, 150, 50);
+		panel_2.add(PouparDinheiro);
 		PouparDinheiro.setIcon(new ImageIcon(TelaPrincipal.class.getResource("/img/pig.png")));
 		
 		JLabel Relatorios = new JLabel("Relat\u00F3rios");
-		panel_2.add(Relatorios, "2, 10, left, center");
+		Relatorios.setBounds(674, 21, 150, 50);
+		panel_2.add(Relatorios);
 		Relatorios.setIcon(new ImageIcon(TelaPrincipal.class.getResource("/img/relatorio..png")));
 		
-		JLabel lblNewLabel = new JLabel("Check-In");
-		lblNewLabel.setIcon(new ImageIcon(TelaPrincipal.class.getResource("/img/marcar.png")));
-		panel_2.add(lblNewLabel, "4, 2, fill, fill");
-		
 		JLabel lblNewLabel_1 = new JLabel("Editar");
-		lblNewLabel_1.setIcon(new ImageIcon(TelaPrincipal.class.getResource("/img/editar3.png")));
-		panel_2.add(lblNewLabel_1, "4, 4, fill, fill");
+		lblNewLabel_1.setBounds(799, 205, 122, 50);
+		lblNewLabel_1.setIcon(new ImageIcon(TelaPrincipal.class.getResource("/img/pencil78.png")));
+		panel_2.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel = new JLabel("Check-In");
+		lblNewLabel.setBounds(799, 140, 122, 50);
+		lblNewLabel.setIcon(new ImageIcon(TelaPrincipal.class.getResource("/img/check.png")));
+		panel_2.add(lblNewLabel);
 		
 		JLabel lblApagar = new JLabel("Apagar");
+		lblApagar.setBounds(799, 266, 122, 50);
 		lblApagar.setIcon(new ImageIcon(TelaPrincipal.class.getResource("/img/apagar.png")));
-		panel_2.add(lblApagar, "4, 6, fill, fill");
+		panel_2.add(lblApagar);
 		
 		JPanel panel_3 = new JPanel();
 		panel_3.setBorder(new TitledBorder(null, "Linha do Tempo", TitledBorder.LEADING, TitledBorder.TOP, null, null));
